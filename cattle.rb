02a3c1f91c65633c4ea221d3efaf06c4b1b276e9
aaron <37416260+itscomputers@users.noreply.github.com>
2020-02-28 16:17:17 -0700

    fix tabs

--- cattle.rb
+++ cattle.rb
@@ -1,87 +1,87 @@
 class ContinuedFraction
-	attr_reader :convergents
+  attr_reader :convergents
 
-	def initialize(root)
-		@root = root
-		@approx = Math.sqrt(root).floor
-		@alpha = [0, 1, 1]
-		@convergents = [[0, 1], [1, 0]]
-	end
+  def initialize(root)
+    @root = root
+    @approx = Math.sqrt(root).floor
+    @alpha = [0, 1, 1]
+    @convergents = [[0, 1], [1, 0]]
+  end
 
-	def quotient
-		a, b, c = @alpha
-		(a + b * @approx) / c
-	end
+  def quotient
+    a, b, c = @alpha
+    (a + b * @approx) / c
+  end
 
-	def next_alpha
-		a, b, c = @alpha
-		r = quotient * c - a
-		alpha = [r * c, b * c, b**2 * @root - r**2]
-		d = alpha.reduce(&:gcd)
-		alpha.map { |x| x / d }
-	end
+  def next_alpha
+    a, b, c = @alpha
+    r = quotient * c - a
+    alpha = [r * c, b * c, b**2 * @root - r**2]
+    d = alpha.reduce(&:gcd)
+    alpha.map { |x| x / d }
+  end
 
-	def next_convergent
-		@convergents.reduce(&:zip).map { |(prev, curr)| prev + quotient * curr }
-	end
+  def next_convergent
+    @convergents.reduce(&:zip).map { |(prev, curr)| prev + quotient * curr }
+  end
 
-	def advance
-		@convergents = [*@convergents, next_convergent].drop(1)
-		@alpha = next_alpha
-	end
+  def advance
+    @convergents = [*@convergents, next_convergent].drop(1)
+    @alpha = next_alpha
+  end
 
-	def run
-		advance until @alpha == [@approx, 1, 1]
-		self
-	end
+  def run
+    advance until @alpha == [@approx, 1, 1]
+    self
+  end
 end
 
 class QuadraticInteger
-	attr_reader :real, :imag, :root
+  attr_reader :real, :imag, :root
 
-	def initialize(real, imag, root)
-		@real = real
-		@imag = imag
-		@root = root
-	end
+  def initialize(real, imag, root)
+    @real = real
+    @imag = imag
+    @root = root
+  end
 
-	def mult(other)
-		real = @real * other.real + @imag * other.imag * @root
-		imag = @real * other.imag + @imag * other.real
-		QuadraticInteger.new(real, imag, @root)
-	end
+  def mult(other)
+    real = @real * other.real + @imag * other.imag * @root
+    imag = @real * other.imag + @imag * other.real
+    QuadraticInteger.new(real, imag, @root)
+  end
 	
-	def power(exponent)
-		return QuadraticInteger.new(1, 0, @root) if exponent == 0
-		return self if exponent == 1
+  def power(exponent)
+    return QuadraticInteger.new(1, 0, @root) if exponent == 0
+    return self if exponent == 1
 		
-		half_power = power(exponent / 2)
-		return half_power.mult(half_power) if exponent % 2 == 0
-		mult(half_power.mult(half_power))
-	end
+    half_power = power(exponent / 2)
+    return half_power.mult(half_power) if exponent % 2 == 0
+    mult(half_power.mult(half_power))
+  end
 end
 
 class CattleProblem
-	def initialize
-		@part_one = 50389082
-		@root = 4729494
-	end
+  def initialize
+    @part_one = 50389082
+    @root = 4729494
+  end
 
-	def first_solution
-		ContinuedFraction.new(@root).run.convergents.last
-	end
+  def first_solution
+    ContinuedFraction.new(@root).run.convergents.last
+  end
 
-	def y_value
-		QuadraticInteger.new(*first_solution, @root).power(2329).imag
-	end
+  def y_value
+    QuadraticInteger.new(*first_solution, @root).power(2329).imag
+  end
 
-	def multiplier
-		y_value**2 * 957 / 18628
-	end
+  def multiplier
+    y_value**2 * 957 / 18628
+  end
 
-	def part_two
-		(@part_one * multiplier).to_s
-	end
+  def part_two
+    (@part_one * multiplier).to_s
+  end
 end
 
 SOLUTION = CattleProblem.new.part_two
